8d2d7d26a543c414aef50d3721d2c973d38f3e5e
Ali Hussein <ah0979075074@gmail.com>
2024-10-20 14:45:27 +0300

    edited

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,4 @@
-SELECT * FROM books;
+SELECT column_name, data_type, character_maximum_length, is_nullable
+FROM information_schema.columns
+WHERE table_schema = 'alx_book_store'
+AND table_name = 'books';
